59549e403ec069819361bc3c7addcd103ee54ca0
Olaf van Venetie <olafvv@Olafs-MacBook-Pro.local>
2020-12-13 13:43:59 +0100

    cypress 3

--- cypress/integration/more-heroes.spec.js
+++ cypress/integration/more-heroes.spec.js
@@ -1,8 +1,18 @@
 describe('More Heroes page tests', () => {
 
-  // it('should show Superheroes as title of the page', () => {
-  // });
+  it('should show Superheroes as title of the page', () => {
+    cy.visit('/');
+    cy.get('.title').contains('Superheroes');
+  });
 
-  // it('should show an empty state when there are no heroes', () => {
-  // });
+  it('should show an empty state when there are no heroes', () => {
+    // cy.server();
+    // cy.route('/superhero-api/api/all.json', 'fixture:allheroes');
+
+    cy.intercept('GET', '/superhero-api/api/all.json', {fixture: 'allheroes'}).as('getAllHeroes');
+
+    cy.visit('/');
+    cy.wait('@getAllHeroes');
+    cy.get('.empty-state').should('be.visible');
+  });
 });
